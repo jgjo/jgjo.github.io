601bd9d6d48eb57175c859f17499c59481d0bcf1
Janet Johnson <janet.johnson8609@gmail.com>
2017-03-14 02:36:32 -0700

    js change

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -28,9 +28,9 @@
             /*====================================
     FOR IMAGE/GALLERY POPUP
     ======================================*/
-            $("a.preview").prettyPhoto({
-                social_tools: false
-            });
+            // $("a.preview").prettyPhoto({
+            //     social_tools: false
+            // });
             /*====================================
           FOR IMAGE/GALLERY FILTER
           ======================================*/
@@ -38,14 +38,14 @@
             // MixItUp plugin
             // http://mixitup.io
 
-            $('#port-folio').mixitup({
-                targetSelector: '.portfolio-item',
-                filterSelector: '.filter',
-                effects: ['fade'],
-                easing: 'snap',
+            // $('#port-folio').mixitup({
+            //     targetSelector: '.portfolio-item',
+            //     filterSelector: '.filter',
+            //     effects: ['fade'],
+            //     easing: 'snap',
 
 
-            });
+            // });
         },
        
         custom_fun:function()
